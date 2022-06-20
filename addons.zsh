fc4765a050683ce604fe1cf339526892bda8c9c5
Alejandro Anzola <anzola95avila@gmail.com>
2022-06-19 19:40:20 -0500

    small refactor

--- addons.zsh
+++ addons.zsh
@@ -1,9 +1,5 @@
 # iTerm
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-# pyenv
 source $ZSH_LOCATION/addons/pyenv.zsh
-
-
-# sdkman
 source $ZSH_LOCATION/addons/sdkman.zsh

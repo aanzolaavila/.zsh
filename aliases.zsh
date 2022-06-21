046f94195794f212dd814288a8502ba26a29dfb2
Alejandro Anzola-Avila <aanzolaavila@twilio.com>
2022-06-21 10:15:39 -0500

    changes alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,7 @@
 alias v="nvim"
 alias vim="nvim"
 alias vconf="pushd ~/.vim; nvim; popd"
-alias vref="pushd ~/Documents/learning/learning-vim/; nvim *.md; popd"
+alias vref="pushd ~/Documents/learning/learning-vim/ && nvim *.md && popd"
 alias doc="docker"
 alias zconf="nvim ~/.zshrc"
 alias notify='terminal-notifier -title "Terminal" -message'

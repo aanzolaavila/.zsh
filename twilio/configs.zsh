abeb47294d5513d8911f095419b9a5339c86be1b
Alejandro Anzola-Avila <aanzolaavila@twilio.com>
2022-06-22 10:57:21 -0500

    adds jenv inside concurrent function calls

--- twilio/configs.zsh
+++ twilio/configs.zsh
@@ -2,7 +2,13 @@
 export GOPRIVATE="code.hq.twilio.com"
 
 # jEnv
-export PATH="$HOME/.jenv/bin:$PATH"
-_evalcache jenv init -
+function _zsh_load_jenv() {
+  export PATH="$HOME/.jenv/bin:$PATH"
+  _evalcache jenv init -
+}
+
+# already defined in addons.zsh
+# async_start_worker addons_worker -n
+async_job addons_worker _zsh_load_jenv
 
 export SUMO_HOME=/usr/local/opt/sumo/share/sumo

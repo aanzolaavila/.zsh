f71782c3ddde7025ce3cc467a64334bf3ee6c472
Alejandro Anzola-Avila <aanzolaavila@twilio.com>
2022-06-24 12:23:14 -0500

    adds zsh workaround for tmux zsh interactive non-login

--- globals.zsh
+++ globals.zsh
@@ -1,3 +1,6 @@
 # Where .zimrc is
 export ZDOTDIR="$ZSH_LOCATION/zim"
+
+# zsh interactive non-login workaround
+[[ ! -f "$ZDOTDIR/.zshrc" ]] && ln -s $HOME/.zshrc $ZDOTDIR/.zshrc
 export PURE_CHECK_SSH=false

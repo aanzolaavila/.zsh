650bc9231a3c4d6ee1f8ee6b5391110592612642
Alejandro Anzola <anzola95avila@gmail.com>
2022-11-26 23:08:33 -0500

    asdf script is changed

--- addons/asdf.zsh
+++ addons/asdf.zsh
@@ -1,9 +1,16 @@
 function _zsh_load_asdf() {
   local asdf_dir="$HOME/.asdf"
   local plugin_dir="$asdf_dir/plugins"
+  local tool_versions_loc="$HOME/.tool-versions"
+
   local new_plugin=false
 
-  local plugins_str="$(cat $HOME/.tool-versions | awk '{print $1}')"
+  if [[ ! -e $tool_versions_loc ]]; then
+    echo "Could not find $tool_versions_loc"
+    return
+  fi
+
+  local plugins_str="$(cat $tool_versions_loc | awk '{print $1}')"
   local plugins=("${(f)plugins_str}")
 
   for plugin in $plugins; do

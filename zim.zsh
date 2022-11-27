dbfb765324fc9abceb298b77cae3a86271eef95d
Alejandro Anzola <anzola95avila@gmail.com>
2022-11-26 23:59:54 -0500

    removes autodownload of zimfw

--- zim.zsh
+++ zim.zsh
@@ -1,13 +1,10 @@
-export ZIM_HOME="$HOME/.cache/zim"
-
 function _zsh_load_zim(){
   # Workaround for now
   unset ZDOTDIR
 
-  # Download zimfw plugin manager if missing.
+  # Check if zimfw plugin manager is missing.
   if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-    curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+    echo "error: no zimfw plugin manager is available: not found in $ZIM_HOME"
   fi
 
   # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.

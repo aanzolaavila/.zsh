7471c7c3276b17abf452c83c8f7dd48d7c0da14b
Alejandro Anzola <anzola95avila@gmail.com>
2022-11-27 00:56:25 -0500

    sets sdkman to be sourced from already made installation, instead of doing automatic installation

--- addons/sdkman.zsh
+++ addons/sdkman.zsh
@@ -1,11 +1,4 @@
-export SDKMAN_DIR="$HOME/.sdkman"
-
 function _zsh_load_sdkman() {
-  if [[ ! -d "$SDKMAN_DIR" ]]; then
-    echo "SDKMAN is not installed, lets install it"
-    curl -s "https://get.sdkman.io" | bash
-  fi
-
   if [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then
     source "$SDKMAN_DIR/bin/sdkman-init.sh"
   fi

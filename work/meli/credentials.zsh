8bbe543df115de49ace873df0f0699deede66717
Alejandro Anzola <anzola95avila@gmail.com>
2022-12-21 10:33:29 -0500

    sync bd credentials with main meli work credentials

--- work/meli/credentials.zsh
+++ work/meli/credentials.zsh
@@ -1,6 +1,8 @@
 function _zsh_sync_work_credentials(){
   local mlmail="ceac0274-edc8-43b7-a3df-af4100ff5e2e"
   local mladmin="2ef40c52-304e-4cea-bc2c-af4700f5fffb"
+  local melisegifdb="a3c05bd8-161a-4f47-ac09-af6400fe96c4"
 
   sync_bw_item_password $mlmail $mladmin
+  sync_bw_item_password $mlmail $melisegifdb
 }

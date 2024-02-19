c11fd7d64c7ebdf7cce5b3f4ba65618e51b298ee
Alejandro Anzola <me@alejandroanzola.info>
2024-02-19 16:42:53 -0500

    adds a wezterm config to setup a random wallpaper on TERM startup

--- addons.zsh
+++ addons.zsh
@@ -3,10 +3,12 @@ source $ZSH_LOCATION/addons/pyenv.zsh
 source $ZSH_LOCATION/addons/sdkman.zsh
 source $ZSH_LOCATION/addons/asdf.zsh
 source $ZSH_LOCATION/addons/sgpt.zsh
+source $ZSH_LOCATION/addons/wezterm.zsh
 
 confunctions+=_zsh_load_iterm
 confunctions+=_zsh_load_pyenv
 confunctions+=_zsh_load_asdf
+confunctions+=_zsh_load_wezterm
 
 deferfunctions+=_zsh_load_sdkman
 deferfunctions+=_zsh_load_sgpt
---
+++ addons/wezterm.zsh
@@ -0,0 +1,6 @@
+function _zsh_load_wezterm() {
+  # if not inside tmux, change the wezterm background wallpaper
+  if [[ -z "$TMUX" ]]; then
+    $ZSH_LOCATION/scripts/shuffle-wezterm-wall
+  fi
+}

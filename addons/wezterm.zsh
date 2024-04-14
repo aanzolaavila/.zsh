2d2525336a3915df66d1b2a6d7e44da1e7eeecb3
Alejandro Anzola <me@alejandroanzola.info>
2024-04-14 02:01:15 -0500

    Updates wezterm script to be generic
    This to be ubiquous

--- addons/wezterm.zsh
+++ addons/wezterm.zsh
@@ -1,6 +1,6 @@
 function _zsh_load_wezterm() {
   # if not inside tmux, change the wezterm background wallpaper
   if [[ -z "$TMUX" ]]; then
-    $ZSH_LOCATION/scripts/shuffle-wezterm-wall
+    $ZSH_LOCATION/scripts/shuffle-term-wall
   fi
 }

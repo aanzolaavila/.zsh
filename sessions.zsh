44aa35408ef0287e3d3fa9ba9f40e6c9e0f3ecaa
Alejandro Anzola <me@alejandroanzola.info>
2025-05-05 08:18:24 -0500

    Adds zellij taz configuration

--- sessions.zsh
+++ sessions.zsh
@@ -4,8 +4,8 @@ _not_inside_zellij() { [[ -z "$ZELLIJ" ]]; }
 ensure_zellij_is_running() {
   if _not_inside_zellij; then
     taz "${HOME}"
+    zellij attach "${USER}"
   fi
 }
 
 ensure_zellij_is_running
-zellij attach "${USER}"

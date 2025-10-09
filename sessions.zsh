3ab973c181b25bf8664a0bae996d0fbc3ade475e
Alejandro Anzola <me@alejandroanzola.info>
2025-10-09 00:26:03 -0500

    Creates and modifies some scripts to handle multiple TMUX servers

--- sessions.zsh
+++ sessions.zsh
@@ -1,9 +1,30 @@
+DEFAULT_LOCATION="/private/tmp"
+
+INITIAL_SERVERS=("default" "personal")
+
 # Always run tmux by default
 _not_inside_tmux() { [[ -z "$TMUX" ]]; }
 
+get_servers() {
+  local uid
+  local servers
+  uid="$(id -u)"
+  pushd "${DEFAULT_LOCATION}/tmux-${uid}" >/dev/null || exit 1
+  servers=("$(ls -l1)")
+  popd >/dev/null || exit 1
+  echo "${servers[@]}"
+}
+
 ensure_tmux_is_running() {
-  if _not_inside_tmux; then
-    tat "${HOME}"
+  local servers_count=($(wc -w <<<"$(get_servers)"))
+  if [ "${servers_count}" -eq 0 ]; then
+    for server in "${INITIAL_SERVERS[@]}"; do
+      tmux -L "${server}" start
+    done
+
+    if _not_inside_tmux; then
+      tat "${HOME}"
+    fi
   fi
 }
 
